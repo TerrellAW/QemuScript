c2d10cfde6e523c5c8405d646b5386afbf8ccbcf
TerrellAW <terraw1999@proton.me>
2026-02-03 12:15:18 -0700

    Initial commit

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Uncomment for piping in actual CD/DVD drive
+#CDROM=""
+#[[ -b "/dev/sr0" ]] && CDROM="-cdrom /dev/sr0"
+
+qemu-system-x86_64 \
+  -m 4096 \
+  -cpu host \
+  -smp 2 \
+  -enable-kvm \
+  -cdrom install.iso \
+  #$CDROM \ # Uses commented CDROM variable
+  -boot order=d \ # Change to order=c after successful install
+  -drive file=osname.qcow2,format=qcow2 \ # Generate 50G Virtual HDD with qemu-img create -f qcow2 osname.qcow2 50G
+  -machine type=pc \
+  -vga std \
+  -net nic \
+  -net user \
+  -device AC97 \
+  -usb -device usb-tablet \
+  -name "OSName VM"
