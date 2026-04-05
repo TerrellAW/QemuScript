ece6085e5b9f2c326892f964c105cf8e9ec60570
Bones <terraw1999@proton.me>
2026-04-05 17:59:27 -0600

    Change: Align backslashes

--- script.sh
+++ script.sh
@@ -4,22 +4,22 @@
 #CDROM=""
 #[[ -b "/dev/sr0" ]] && CDROM="-cdrom /dev/sr0"
 
-qemu-system-x86_64 \
-  -m 4096 \
-  -cpu host \
-  -smp 2 \
-  -enable-kvm \
-  -cdrom install.iso \
+qemu-system-x86_64                      \
+  -m 4096                               \
+  -cpu host                             \
+  -smp 2                                \
+  -enable-kvm                           \
+  -cdrom install.iso                    \
   # Uses commented CDROM variable
-  #$CDROM \
+  #$CDROM                               \
   # Change to order=c after successful install
-  -boot order=d \
+  -boot order=d                         \
   # Generate 50G Virtual HDD with qemu-img create -f qcow2 osname.qcow2 50G
   -drive file=osname.qcow2,format=qcow2 \
-  -machine type=pc \
-  -vga std \
-  -net nic \
-  -net user \
-  -device AC97 \
-  -usb -device usb-tablet \
+  -machine type=pc                      \
+  -vga std                              \
+  -net nic                              \
+  -net user                             \
+  -device AC97                          \
+  -usb -device usb-tablet               \
   -name "OSName VM"
